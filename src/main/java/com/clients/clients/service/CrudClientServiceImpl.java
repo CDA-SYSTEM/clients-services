90fc78dfe677aa2dcf1255975fa93d872c821c5e
Ilesandres <ilesandres6@gmail.com>
2026-05-09 16:46:37 -0500

    fix(api): 🐛 add Transactional annotation to CrudClientServiceImpl

--- src/main/java/com/clients/clients/service/CrudClientServiceImpl.java
+++ src/main/java/com/clients/clients/service/CrudClientServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class CrudClientServiceImpl implements CrudClientService {
     private final ClientRepository clientRepository;
     private final CrudPersonTypeService personTypeService;
