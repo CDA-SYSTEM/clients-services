13f98c3aa01a93d9802da95c54bda3078e93cd22
Ilesandres <ilesandres6@gmail.com>
2026-05-09 16:46:25 -0500

    chore(api): 🔧 add Transactional import to CrudClientServiceImpl

--- src/main/java/com/clients/clients/service/CrudClientServiceImpl.java
+++ src/main/java/com/clients/clients/service/CrudClientServiceImpl.java
@@ -5,6 +5,7 @@ import com.clients.clients.repository.ClientRepository;
 import com.clients.clients.personType.CrudPersonTypeService;
 import com.clients.clients.documentType.CrudDocumentTypeService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
