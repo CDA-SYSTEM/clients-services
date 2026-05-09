11e7566ab43ac79c259d4969c8184c5898ac2af4
Ilesandres <ilesandres6@gmail.com>
2026-05-09 16:47:23 -0500

    chore(api): 🔧 add Valid import to ClientController

--- src/main/java/com/clients/clients/controller/ClientController.java
+++ src/main/java/com/clients/clients/controller/ClientController.java
@@ -14,6 +14,7 @@ import com.clients.shared.dto.NotFoundErrorDTO;
 import com.clients.shared.utils.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
